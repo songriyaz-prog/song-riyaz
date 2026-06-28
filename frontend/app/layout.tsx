aa9aab033b5059bd17d8cc4c281de082416f56e2
Haroon Ashraf <hansloji20@gmail.com>
2026-06-29 00:20:46 +0500

    Add Google Analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -77,7 +78,25 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DH93D4SGHH"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DH93D4SGHH');
+          `}
+        </Script>
+
+        {children}
+
+      </body>
     </html>
   );
 }
